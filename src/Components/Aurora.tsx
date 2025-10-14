a47f4d71bed918a6d432ccaeadf04565635b4735
protik0939 <protik0939@gmail.com>
2025-10-15 03:15:02 +0600

    fix: initialize program variable to undefined in Aurora component

--- src/Components/Aurora.tsx
+++ src/Components/Aurora.tsx
@@ -138,8 +138,7 @@ export default function Aurora(props: AuroraProps) {
     gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
     gl.canvas.style.backgroundColor = 'transparent';
 
-    // eslint-disable-next-line prefer-const
-    let program: Program | undefined;
+    let program: Program | undefined = undefined;
 
     function resize() {
       if (!ctn) return;
